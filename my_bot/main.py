d89c22137ebaaf26e6bb7b29c06495f4348dad54
EugeniiPetrov <epoy74@gmail.com>
2025-04-27 02:05:43 +0500

    Initial Telegram bot startup code

--- my_bot/main.py
+++ my_bot/main.py
@@ -1,5 +1,34 @@
 """
 Основной файл телеграмм бота
-Автор: ЕВгений Петров
+Автор: Евгений Петров
 Почта: p174@mail.ru
 """
+
+
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+load_dotenv()
+bot = Bot(token=str(os.getenv("BOT_TOKEN")))
+dp = Dispatcher()
+
+
+async def main():
+    """
+    Оcновная функция для запуска
+    """
+
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    """
+    Запуск кода в асинхронном иде
+    """
+
+    asyncio.run(main())
