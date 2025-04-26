58955d3f539785a20e06c85b632b71f32009815d
EugeniiPetrov <epoy74@gmail.com>
2025-04-27 02:16:54 +0500

    First implementation of Telegram bot

--- my_bot/main.py
+++ my_bot/main.py
@@ -4,7 +4,6 @@
 Почта: p174@mail.ru
 """
 
-
 import asyncio
 import logging
 import os
@@ -12,6 +11,11 @@ import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+)
 load_dotenv()
 bot = Bot(token=str(os.getenv("BOT_TOKEN")))
 dp = Dispatcher()
@@ -19,16 +23,16 @@ dp = Dispatcher()
 
 async def main():
     """
-    Оcновная функция для запуска
+    Оcновная функция для запуска    
     """
-
-    logging.basicConfig(level=logging.INFO)
+    
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     """
-    Запуск кода в асинхронном иде
+    Запуск кода в асинхронном dиде
     """
 
+    logging.info("Запуск телеграм бота")
     asyncio.run(main())
