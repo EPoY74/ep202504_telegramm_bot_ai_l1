4814cc693b8b7bb1807c60c64b08f9f9025a2e7e
EugeniiPetrov <epoy74@gmail.com>
2025-04-27 18:36:46 +0500

    Refactoring, added async  function for closing dp.storage and bot.session

--- my_bot/main.py
+++ my_bot/main.py
@@ -7,6 +7,7 @@
 import asyncio
 import logging
 import os
+from builtins import ExceptionGroup
 
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
@@ -22,6 +23,7 @@ class EnvLoadError(Exception):
     """
     Класс для пробрасывания исключений
     """
+
     pass
 
 
@@ -29,7 +31,14 @@ class Bot_Config:
     """
     Загружает конфигурацию Телеграмм бота
     """
+
     def __init__(self) -> None:
+        """
+        Docstring for __init__
+
+        :param self: Description
+        :type self:
+        """
         if not load_dotenv():
             err_msg = ".env не загружен или не найден"
             logging.warning(err_msg)
@@ -40,11 +49,12 @@ class Bot_Config:
         except ValueError as err:
             logging.critical(f"Отсутствует BOT_TOKEN в .env файле: {err}")
             raise ValueError("Токен телеграм бота не найден") from err
-        bot_admins: str|None = os.getenv("ADMINS_ID")
+        bot_admins: str | None = os.getenv("ADMINS_ID")
         if bot_admins:
             try:
-                self.ADMINS_ID = [int(bot_admin_id.strip())
-                                   for bot_admin_id in bot_admins.split(",")
+                self.ADMINS_ID = [
+                    int(bot_admin_id.strip())
+                    for bot_admin_id in bot_admins.split(",")
                 ]
             except ValueError as err:
                 err_msg = (
@@ -53,33 +63,42 @@ class Bot_Config:
                 )
                 logging.error(err_msg)
                 self.ADMINS_ID = []
-                raise ValueError(err_msg) from err   
+                raise ValueError(err_msg) from err
+
+    # @staticmethod
+    # def load_config() -> Bot_Config | None:
+    #     # ...
 
 
-async def shutdown(dp: Dispatcher, bot: Bot):
+async def shutdown(dp: Dispatcher, bot: Bot) -> None:
     """
     Завершение телеграмм бота
     """
 
-    logging.warning("Завершение работы телеграмм бота")
-    
-    try:
-        await dp.storage.close()
-    except Exception as err:
-        err_msg=f"Ошибка закрытия storage: {err}"
-        logging.error(err_msg)
-        raise Exception(err_msg) from Exception
+    logging.info("Завершение работы телеграмм бота")
 
-    try:
-        await bot.session.close()
-    except Exception as err:
-        err_msg = f"Ошибка закрытия сессии: {err}"
-        logging.error(err_msg)
-        raise Exception(err_msg) from Exception
+    shutdown_results = await asyncio.gather(
+        #  Закрываю асинхронно хранилище и сессию
+        #  что бы корректно освободить ресурсы и обработать исключения
+        dp.storage.close(),
+        bot.session.close(),
+        #  Что бы не падало при ошибках, устанавляваем True
+        return_exceptions=True,
+    )
 
-async def main():
+    result_shutdown_errors: list = []
+    for closed_item_status in shutdown_results:
+        for closed_item_status in shutdown_results:
+            if isinstance(closed_item_status, Exception):
+                logging.error(f"Ошибка при закрытии: {closed_item_status}")
+                result_shutdown_errors.append(closed_item_status)
+        if len(result_shutdown_errors) > 0:
+            raise ExceptionGroup("Ошибки при закрытии бота", result_shutdown_errors)
+
+
+async def main() -> None:
     """
-    Оcновная функция для запуска    
+    Оcновная функция для запуска телеграм бота
     """
     config = Bot_Config()
     bot = Bot(config.BOT_TOKEN)
@@ -98,15 +117,14 @@ async def main():
 
 if __name__ == "__main__":
     """
-    Запуск кода в асинхронном виде
+    Запуск скрипта в асинхронном виде
     """
 
     try:
         asyncio.run(main())
-    except(KeyboardInterrupt, SystemExit) as err:
+    except (KeyboardInterrupt, SystemExit) as err:
         logging.info(f"Бот остановлен вручную: {err}")
     except Exception as err:
         err_msg = f"Критическая ошибка: {err}"
         logging.critical(err_msg)
         raise Exception(err_msg) from Exception
-
