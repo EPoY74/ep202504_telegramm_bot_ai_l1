ea458c8c1062439dedf1f2f15039268071d8f067
EugeniiPetrov <epoy74@gmail.com>
2025-05-27 22:37:49 +0500

    Создал модель users

---
+++ uk_bot/bot/models/user.py
@@ -0,0 +1,14 @@
+"""
+Модель пользователя
+"""
+
+from bot.db import Base
+from sqlalchemy import BigInteger, Column, String
+
+class User(Base):
+    __tablenname__ = "users"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    
