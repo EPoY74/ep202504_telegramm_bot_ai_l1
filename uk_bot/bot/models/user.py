4fefbf7049f9fa57316b3d627eca0702412a81ed
EugeniiPetrov <epoy74@gmail.com>
2025-05-28 07:20:18 +0500

    Промежуточное сохранение

--- uk_bot/bot/models/user.py
+++ uk_bot/bot/models/user.py
@@ -5,10 +5,10 @@
 from bot.db import Base
 from sqlalchemy import BigInteger, Column, String
 
+
 class User(Base):
     __tablenname__ = "users"
 
     id = Column(BigInteger, primary_key=True, index=True)
     full_name = Column(String, nullable=False)
     phone = Column(String, nullable=False)
-    
