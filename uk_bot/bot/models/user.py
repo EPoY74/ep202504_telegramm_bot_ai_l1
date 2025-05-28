2fdcba2b95effcd5705d0c72e6f206a1017a72e8
EugeniiPetrov <epoy74@gmail.com>
2025-05-28 14:54:58 +0500

    Поправил для работы с актуальной версией  Алхимии

--- uk_bot/bot/models/user.py
+++ uk_bot/bot/models/user.py
@@ -3,12 +3,13 @@
 """
 
 from bot.db import Base
-from sqlalchemy import BigInteger, Column, String
+from sqlalchemy import BigInteger, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(Base):
     __tablenname__ = "users"
 
-    id = Column(BigInteger, primary_key=True, index=True)
-    full_name = Column(String, nullable=False)
-    phone = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    full_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
